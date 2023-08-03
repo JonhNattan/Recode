ae8d1a373e55d6989283c9bfcb705c08b86d84ca
JonhNattan <jonhcaxias@gmailcom>
2023-08-03 12:26:29 -0300

    first commit

---
+++ Js/js.js
@@ -0,0 +1,11 @@
+const pessoa = {
+    nome: '',
+    sobrenome: ' ',
+    idade: ,
+  };
+
+pessoa.nome = prompt('Digite o nome:');
+pessoa.sobrenome = prompt('Digite o sobrenome:');
+pessoa.idade = parseInt(prompt('Digite a idade:'));
+
+alert(`Nome: ${pessoa.nome}\nSobrenome: ${pessoa.sobrenome}\nIdade: ${pessoa.idade}`);
